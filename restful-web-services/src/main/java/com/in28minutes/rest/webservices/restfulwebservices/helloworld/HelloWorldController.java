4aac319b3a7acf3f2755685a7c5a6fd9a6f90910
jeong <chung_junhuck@infonic.co.jp>
2023-12-21 23:45:33 +0900

    i18n - message properties and header:accept-language

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 @RestController
 public class HelloWorldController {
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	// give a specific url: /hello-world
 	
 	// return "Hello World"
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	// i18n ~ the meaning is internationalization
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		// return "Hello World V2";
 	}
 	
 	@GetMapping(path = "/hello-world-bean")
